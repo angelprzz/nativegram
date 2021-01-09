c6e4553c9072615f8e6c19df82c4a540f247f83d
Ángel Pérez <angelpxrz@gmail.com>
2021-01-09 21:47:48 +0100

    Pick picture from the gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
     })();
   }, []);
 
@@ -22,10 +27,24 @@ export default function App() {
     }
   };
 
-  if (hasPermission === null) {
-    return <View />;
+  const pickPicture = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
+    return <Text>lel</Text>;
   }
-  if (hasPermission === false) {
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
@@ -53,6 +72,12 @@ export default function App() {
         title="Take Picture"
         onPress={() => {
           takePicture();
+        }}
+      />
+      <Button
+        title="Pick Image From Gallery"
+        onPress={() => {
+          pickPicture();
         }}
       />
       {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
