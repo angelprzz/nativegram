09378dd05c9aaec7ed9d1ec1d2f7cdca51a74653
Ángel Pérez <angelpxrz@gmail.com>
2021-01-18 17:48:10 +0100

    Indentation fixes

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -6,27 +6,27 @@ admin.initializeApp();
 const db = admin.firestore();
 
 exports.addLike = functions.firestore
-  .document("/posts/{creatorId}/userPosts/{postId}/likes/{userId}")
-  .onCreate((snap, context) => {
-    return db
-      .collection("posts")
-      .doc(context.params.creatorId)
-      .collection("userPosts")
-      .doc(context.params.postId)
-      .update({
-        likesCount: admin.firestore.FieldValue.increment(1),
-      });
-  });
+    .document("/posts/{creatorId}/userPosts/{postId}/likes/{userId}")
+    .onCreate((snap, context) => {
+      return db
+          .collection("posts")
+          .doc(context.params.creatorId)
+          .collection("userPosts")
+          .doc(context.params.postId)
+          .update({
+            likesCount: admin.firestore.FieldValue.increment(1),
+          });
+    });
 
 exports.removeLike = functions.firestore
-  .document("/posts/{creatorId}/userPosts/{postId}/likes/{userId}")
-  .onCreate((snap, context) => {
-    return db
-      .collection("posts")
-      .doc(context.params.creatorId)
-      .collection("userPosts")
-      .doc(context.params.postId)
-      .update({
-        likesCount: admin.firestore.FieldValue.increment(-1),
-      });
-  });
+    .document("/posts/{creatorId}/userPosts/{postId}/likes/{userId}")
+    .onDelete((snap, context) => {
+      return db
+          .collection("posts")
+          .doc(context.params.creatorId)
+          .collection("userPosts")
+          .doc(context.params.postId)
+          .update({
+            likesCount: admin.firestore.FieldValue.increment(-1),
+          });
+    });
